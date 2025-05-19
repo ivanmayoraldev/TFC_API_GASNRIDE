d5030c9bc91e322b9523cdf0887e430ed9181ba2
ivanmayoraldev <ivanmayoral.dev@gmail.com>
2025-05-20 00:02:48 +0200

    fixes api

--- index.js
+++ index.js
@@ -8,16 +8,16 @@ const path = require("path");
 admin.initializeApp({
   credential: admin.credential.cert({
     type: "service_account",
-    project_id: FIREBASE_PROJECT_ID,
-    private_key_id: FIREBASE_PRIVATE_KEY_ID,
-    private_key: FIREBASE_PRIVATE_KEY,
-    client_email: FIREBASE_CLIENT_EMAIL,
-    client_id: FIREBASE_CLIENT_ID,
-    auth_uri: FIREBASE_AUTH_URI,
-    token_uri: FIREBASE_TOKEN_URI,
-    auth_provider_x509_cert_url: FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-    client_x509_cert_url: FIREBASE_CLIENT_X509_CERT_URL,
-    universe_domain: FIREBASE_UNIVERSE_DOMAIN,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY,
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+    universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
   }),
 });
 
